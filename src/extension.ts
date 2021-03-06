c34dd80e1b8c38eb8d18e7b4049f895ea0237610
Tyler Leonhardt <me@tylerleonhardt.com>
2021-03-06 13:13:55 -0800

    blah

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,25 @@ import { Client } from "@microsoft/microsoft-graph-client";
 import { TodoTaskList } from '@microsoft/microsoft-graph-types';
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
-	const session = await vscode.authentication.getSession('microsoft', ['Tasks.ReadWrite'], { createIfNone: true });
 
-	const client = Client.init({
-		authProvider: (done) => {
-			done(undefined, session.accessToken);
-		}
+	vscode.extensions.onDidChange(() => {
+		vscode.window.showInformationMessage('extensions changed!');
 	});
 
-	try {
-		const res = await client.api('/me/todo/lists').get() as { '@odata.nextLink': string | null | undefined; value: TodoTaskList[] };
-		res.value.map((v) => vscode.window.showInformationMessage(v.displayName || ""));
-	} catch(e) {
-		await vscode.window.showErrorMessage(e.body);
-	}
+	// const session = await vscode.authentication.getSession('microsoft', ['Tasks.ReadWrite'], { createIfNone: true });
+
+	// const client = Client.init({
+	// 	authProvider: (done) => {
+	// 		done(undefined, session.accessToken);
+	// 	}
+	// });
+
+	// try {
+	// 	const res = await client.api('/me/todo/lists').get() as { '@odata.nextLink': string | null | undefined; value: TodoTaskList[] };
+	// 	res.value.map((v) => vscode.window.showInformationMessage(v.displayName || ""));
+	// } catch(e) {
+	// 	await vscode.window.showErrorMessage(e.body);
+	// }
 }
 
 export function deactivate() {}
